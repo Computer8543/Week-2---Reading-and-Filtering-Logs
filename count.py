d7ff4875742d023668ef5f93bd11f5d2100334c5
Computer8543 <college1b7e@gmail.com>
2025-11-08 10:45:02 -0500

    fix(count): change accept to allow.

--- count.py
+++ count.py
@@ -1,7 +1,7 @@
 import csv
 filename = "firewall_logs_2021_2023.csv"
 deny_count = 0
-accept_count = 0
+allow_count = 0
 
 print("Processing file:", filename)
 
@@ -10,8 +10,8 @@ with open(filename) as csvfile:
     for line in csvreader:
         if "DENY" in line:
             deny_count += 1
-        elif "ACCEPT" in line:
-            accept_count += 1
+        elif "ALLOW" in line:
+            allow_count += 1
 
 print(f"Total DENY entries: {deny_count}")
-print(f"Total ACCEPT entries: {accept_count}")
+print(f"Total ALLOW entries: {allow_count}")
